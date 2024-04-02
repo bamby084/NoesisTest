c1e9078c4b8daa33fb425ebb44599072c1d5db54
Viet Nguyen <bamby084@gmail.com>
2024-04-02 18:09:52 +0700

    Add icons

---
+++ Assets/UI/Icon.cs
@@ -0,0 +1,62 @@
+﻿#if UNITY_5_3_OR_NEWER
+#define NOESIS
+using Noesis;
+#else
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+#endif
+
+namespace Testing
+{
+    public class Icon: Control
+    {
+        static Icon()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(Icon), new FrameworkPropertyMetadata(typeof(Icon)));
+        }
+
+        public static readonly DependencyProperty SymbolProperty = DependencyProperty.Register(
+                nameof(Symbol),
+                typeof(IconType),
+                typeof(Icon),
+                new PropertyMetadata(IconType.Empty));
+
+        public IconType Symbol
+        {
+            get => (IconType)GetValue(SymbolProperty);
+            set => SetValue(SymbolProperty, value);
+        }
+
+        public static readonly DependencyProperty ColorProperty = DependencyProperty.Register(
+            nameof(Color),
+            typeof(SolidColorBrush),
+            typeof(Icon),
+            new PropertyMetadata(new SolidColorBrush(Colors.Black)));
+
+        public SolidColorBrush Color
+        {
+            get => (SolidColorBrush)GetValue(ColorProperty);
+            set => SetValue(ColorProperty, value);
+        }
+
+    }
+
+    public enum IconType
+    {
+        Empty,
+        DashBoard,
+        Book,
+        Resource,
+        Brain,
+        BookOpen,
+        Medal,
+        Setting,
+        Bug,
+        LogOut,
+        Mic,
+        Bell,
+        MagnifyingGlass,
+        Audio
+    }
+}
